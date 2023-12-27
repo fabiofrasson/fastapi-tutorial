115cdf6cd445953e51efc730e0c1926f62c5b483
Fabio Frasson <fabio.frass@gmail.com>
2023-12-27 12:29:56 -0300

    mudança README.md e config p/ debug

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 Run project: uvicorn `main:app --reload`
 
+Caso queira adicionar argumentos ou mudar a porta do modo debug, realize este caminho:
+1. Abrir o painel 'Run and Debug' (`CTRL + Shift + D`);
+2. Clicar em `'create a launch.json file' > 'Python' > 'FastAPI'`; 
+3. O arquivo será criado. Altere a lista de argumentos a partir de `'configuration.args'`:
+    "args": [
+            "main:app",
+            "--reload",
+            "--port",
+            "9000"
+          ],
+
 [OpenAPI Documentation](http://localhost:8000/docs)
 
 [Redoc Documentation](http://localhost:8000/redoc)
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
+import uvicorn
 from fastapi import FastAPI
 from typing import Optional
 from pydantic import BaseModel
 
 app = FastAPI()
 
+
 @app.get("/blog")
 # necessário fazer validação do booleano, caso contrário será lido como string
-def index(limit = 10, published: bool = True, sort: Optional[str] = None):
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
     # definir valor padrão para QP, delimitar o tipo e colocá-lo como opcional
 
     # only get 10 published blog posts
-    if published: 
+    if published:
         return {"data": f"{limit} published blogs from the db"}
     else:
         return {"data": f"{limit} blogs from the db"}
+
 
 @app.get("/blog/unpublished")
 def show_unpublished():
@@ -36,14 +39,20 @@ def show_comments(id: int, limit=10):
     # internal server error
     return {"data": ["1", "2"]}
 
+
 # definição de model
 class Blog(BaseModel):
     title: str
     body: str
     published: Optional[bool]
 
+
 @app.post("/blog")
 # delimitação do parâmetro e tipo desse parâmetro
 def create_blog_post(blog: Blog):
     # string formatada, utilizando os valores enviados na requisição
     return {"data": f"Blog post was created with title: {blog.title}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
