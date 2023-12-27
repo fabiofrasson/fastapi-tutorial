c98da0e22bafe673079b28a256dd1810c45dd254
Fabio Frasson <fabio.frass@gmail.com>
2023-12-27 09:11:56 -0300

    ajuste rota show comments

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def index():
-    return {"data": {"name": "Fabio"}}
+    return {"data": "blog list"}
 
-@app.get("/about")
-def about():
-    return {"data": "about page"}
+
+@app.get("/blog/unpublished")
+def show_unpublished():
+    # fetch unpublished blog posts
+    return {"data": "all unpublished blog posts"}
+
+
+@app.get("/blog/{id}")
+def show_blog_post(id: int):
+    # fetch blog with id = id
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def show_comments(id: int):
+    # fetch comments of blog post with id = id
+    # anteriormente estava 
+    # return {"data": "here are the comments for blog post [" + id + "]"} 
+    # e enviar a requisição retornava 
+    # internal server error
+    return {"data": ["1", "2"]}
