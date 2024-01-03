3a5010955cfef866bb6f9bf10f9ab40b197a968f
Fabio Frasson <fabio.frass@gmail.com>
2024-01-03 13:10:44 -0300

    endpoints para visualizar todos os blogs e recuperar blog por id

--- blog/main.py
+++ blog/main.py
@@ -17,9 +17,21 @@ def get_db():
 
 
 @app.post("/blog")
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    db.add(new_blog)  # adicionar novo blog
+    db.commit()  # enviar as alterações
+    db.refresh(new_blog)  # refresh no banco para refletir as alterações
+    return new_blog  # retonar o blog recém criado
+
+
+@app.get("/blog")
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()  # query para retornar todos os blogs
+    return blogs
+
+
+@app.get("/blog/{id}")
+def get_blog_by_id(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
